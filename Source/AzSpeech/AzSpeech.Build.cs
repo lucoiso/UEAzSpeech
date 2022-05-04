f18c218bad45c67636d669862f603dcaa3bec649
Lucoiso <lucas.oliveiravb@gmail.com>
2022-05-04 10:09:53 -0300

    Add a public definition: WITH_OGGVORBIS

--- Source/AzSpeech/AzSpeech.Build.cs
+++ Source/AzSpeech/AzSpeech.Build.cs
@@ -2,30 +2,31 @@
 // Year: 2022
 // Repo: https://github.com/lucoiso/UEAzSpeech
 
-using System;
 using System.IO;
 using UnrealBuildTool;
 
 public class AzSpeech : ModuleRules
 {
-    public AzSpeech(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	public AzSpeech(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		CppStandard = CppStandardVersion.Cpp17;
-        bEnableExceptions = true;
+		bEnableExceptions = true;
 
-        PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
-        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+		PublicDefinitions.Add("WITH_OGGVORBIS");
 
-        PublicDependencyModuleNames.AddRange(
-            new[]
-            {
-                "Engine",
-                "Core",
-                "CoreUObject",
-                "Projects",
-                "AzureWrapper"
-            }
-        );
-    }
+		PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+
+		PublicDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Engine",
+				"Core",
+				"CoreUObject",
+				"Projects",
+				"AzureWrapper"
+			}
+		);
+	}
 }
