0e65891a896506221ff4eedddc0ee5168f2f289b
lucoiso <lucas.oliveiravb@gmail.com>
2023-01-03 13:46:58 -0300

    Rename libs dirs & adjust android UPL to copy the dependencies #62 #58

--- Source/ThirdParty/AzureWrapper/AzureWrapper.Build.cs
+++ Source/ThirdParty/AzureWrapper/AzureWrapper.Build.cs
@@ -40,10 +40,10 @@ public class AzureWrapper : ModuleRules
 		{
 			AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModuleDirectory, "AzSpeech_UPL_Android.xml"));
 
-			string LibPath = "Arm";
-			if (Target.Architecture.ToLower().Contains("arm64"))
+			string LibPath = "arm64-v8a";
+			if (!string.IsNullOrEmpty(Target.Architecture) && !Target.Architecture.ToLower().Contains("64"))
 			{
-				LibPath = "Arm64";
+				LibPath = "armeabi-armv7a";
 			}
 
 			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "libs", "Android", LibPath, "libMicrosoft.CognitiveServices.Speech.core.so"));
