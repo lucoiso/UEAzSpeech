0ee909d961efeb7a90c9a3ec7774b5956fda6c3e
lucoiso <lucas.oliveiravb@gmail.com>
2023-01-29 12:59:55 -0300

    fix buffer size verification

--- Source/AzSpeech/Private/AzSpeech.cpp
+++ Source/AzSpeech/Private/AzSpeech.cpp
@@ -83,7 +83,7 @@ void OutputLastMicrosoftError()
 	LPWSTR MsgBuffer = nullptr;
 	const size_t BufferSize = FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, ErrorID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&MsgBuffer, 0, nullptr);
 	
-	if (BufferSize == 0)
+	if (BufferSize <= 0)
 	{
 		return;
 	}
